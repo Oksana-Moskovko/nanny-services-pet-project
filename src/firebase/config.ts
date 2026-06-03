ab012f9d7508bb53f40ca08f2abaedd420fe81cc
Oksana <omoskovko98@gmail.com>
2026-06-03 21:40:59 +0300

    made the basis for the nannies page; the Realtime Database is created and connected to the site;

---
+++ src/firebase/config.ts
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+// import { log } from "firebase/firestore/pipelines";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: `${import.meta.env.VITE_PROJECT_ID}.firebaseapp.com`,
+  databaseURL: import.meta.env.VITE_DATABASE_NAME,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: `${import.meta.env.VITE_PROJECT_ID}.firebasestorage.app`,
+  messagingSenderId: import.meta.env.VITE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+console.log(`${app} this app`);
+const auth = getAuth(app);
+console.log(`${auth} this auth`);
+
+
+export const database = getDatabase(app);
--- src/pages/NanniesPage/NanniesPage.tsx
+++ src/pages/NanniesPage/NanniesPage.tsx
@@ -1,9 +1,118 @@
+import { useEffect, useState } from "react";
+import { fetchNannies } from "../../services/nannyService";
 import css from "./NanniesPage.module.css";
 
+export interface Nanny {
+  about: string;
+  avatar_url: string;
+  birthday: string;
+  characters: string[];
+  education: string;
+  experience: string;
+  kids_age: string;
+  location: string;
+  name: string;
+  price_per_hour: number;
+  rating: number;
+  reviews: Review[];
+}
+
+export interface Review {
+  comment: string;
+  rating: number;
+  reviewer: string;
+}
+
 export default function NanniesPage() {
+  const [nannies, setNannies] = useState<Nanny[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    fetchNannies().then((data) => {
+      console.log(data);
+      setNannies(Object.values(data || {}));
+    });
+  }, []);
+
   return (
-    <>
-      <h2 className={css.title}>nannies</h2>
-    </>
+    <section className={css.container}>
+      <ul className={css.list}>
+        {nannies.map((nanny, index) => (
+          <li key={`${nanny.name}-${index}`} className={css.listNannies}>
+            <div className={css.divFrame}>
+              <div className={css.frame}>
+                <img
+                  src={nanny.avatar_url}
+                  alt={nanny.name}
+                  width={96}
+                  height={96}
+                  className={css.photoNanny}
+                />
+              </div>
+            </div>
+            <div className={css.frameInfo}>
+              <div className={css.briefInfoTitle}>
+                <div>
+                  <p>Nanny</p>
+                  <h3 className={css.blackText}>{nanny.name}</h3>
+                </div>
+                <div>
+                  <p className={css.blackText}>
+                    <span className={css.icon}></span>
+                    {nanny.location} <span className={css.iconI}>|</span>{" "}
+                    <span className={css.icon}></span>Rating: {nanny.rating}{" "}
+                    <span className={css.iconI}>|</span> Price / 1 hour:{" "}
+                    <span className={css.price}>{nanny.price_per_hour} $</span>
+                  </p>
+                </div>
+              </div>
+
+              <div className={css.briefInfo}>
+                <p className={css.pill}>
+                  Age: <span className={css.underline}>{nanny.birthday}</span>
+                </p>
+                <p className={css.pill}>
+                  Experience:{" "}
+                  <span className={css.blackText}>{nanny.experience}</span>
+                </p>
+                <p className={css.pill}>
+                  Kids Age:{" "}
+                  <span className={css.blackText}>{nanny.kids_age}</span>
+                </p>
+                <p className={css.pill}>
+                  Characters:{" "}
+                  <span className={css.blackText}>
+                    {nanny.characters
+                      .map(
+                        (character) =>
+                          character.charAt(0).toUpperCase() + character.slice(1)
+                      )
+                      .join(", ")}
+                  </span>
+                </p>
+                <p className={css.pill}>
+                  Education:{" "}
+                  <span className={css.blackText}>{nanny.education}</span>
+                </p>
+              </div>
+              <p>{nanny.about}</p>
+              <button onClick={toggle}>Read more</button>
+              {isOpen && (
+                <ul>
+                  {nanny.reviews.map((review) => (
+                    <li>
+                      <div>{review.reviewer}</div>
+                      <p>{review.comment}</p>
+                    </li>
+                  ))}
+                </ul>
+              )}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
